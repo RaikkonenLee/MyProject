3a8d99f87c731e6cd6cc4ce38f3cb0b50d77b259
Raikkonen Lee <xyz7689@hotmail.com>
2017-04-12 14:33:56 +0800

    增加測試AntiXss的寫法

---
+++ TestAntiXss/TestAntiXss/Controllers/TestController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Security.Application;
+
+namespace TestAntiXss.Controllers
+{
+    public class TestController : ApiController
+    {
+        // GET: api/Test
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Test/5
+        public string Get(string id)
+        {
+            //濾掉javascript的區塊
+            string value = Sanitizer.GetSafeHtmlFragment(id);
+            //濾掉javascript的區塊及html的部分換行
+            string value2 = Sanitizer.GetSafeHtml(id);
+            return id;
+        }
+
+        // POST: api/Test
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Test/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Test/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TestAntiXss/TestAntiXss/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestAntiXss.MvcApplication" Language="C#" %>
