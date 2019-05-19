b91e49c9cf3c4bc0108e2be35b9cf4ba630b4bfe
Raikkonen Lee <leewei0528@gmail.com>
2019-05-19 23:47:22 +0800

    新增MVC5Pattern

---
+++ MVC5Pattern/MVC5Pattern/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5Pattern.Startup))]
+namespace MVC5Pattern
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
